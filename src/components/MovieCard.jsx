08d7c2aabca4b7cd4a0bb599d249910d73e7c0f6
saverio mandarano <saverio.mandarano1@gmail.com>
2026-02-23 17:37:49 +0100

    Aggiunta lista di film nella HomePage con relativo CSS

---
+++ src/components/MovieCard.jsx
@@ -0,0 +1,30 @@
+import { Link } from "react-router-dom";
+
+const MovieCard = () => {
+  return (
+    <div className="card mb-4">
+      <img
+        src="../../public/warner-bros.jpg"
+        className="card-img-top"
+        alt="warner bross img"
+      />
+      <div className="card-body">
+        <h5 className="card-title">Titolo del film</h5>
+        <address>
+          <i>Regista</i>
+        </address>
+        <p className="card-text">
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Soluta
+          ratione veritatis sunt molestias nihil odit totam, quis corporis
+          laboriosam excepturi. Labore vero molestiae eveniet explicabo esse
+          iure laborum nemo odio.
+        </p>
+        <Link to={`movies/1`} className="btn btn-primary">
+          See more
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default MovieCard;
--- src/components/pages/HomePage.jsx
+++ src/components/pages/HomePage.jsx
@@ -1,5 +1,19 @@
+import MovieCard from "../MovieCard";
+
 const HomePage = () => {
-  return <h1>HomePage</h1>;
+  return (
+    <>
+      <h1 className="mt-3">Welcome to "Streaming Community" Website</h1>
+      <h3 className="subtitle">The world's largest pirate film archive</h3>
+      <div className="card-container d-flex flex-wrap">
+        <MovieCard />
+        <MovieCard />
+        <MovieCard />
+        <MovieCard />
+        <MovieCard />
+      </div>
+    </>
+  );
 };
 
 export default HomePage;
