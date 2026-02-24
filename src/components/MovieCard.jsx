bc0147c69f9ada9e25e86f541b8360e1352fbea8
saverio mandarano <saverio.mandarano1@gmail.com>
2026-02-24 16:17:49 +0100

    Aggiunta chiamata Ajax dalla home del progetto React, per ottenere la lista dei film

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,25 +1,18 @@
 import { Link } from "react-router-dom";
 
-const MovieCard = () => {
+const MovieCard = (props) => {
+  const { id, title, author, abstract, image_url } = props.movieProp;
+
   return (
     <div className="card mb-4 homepage">
-      <img
-        src="../../public/warner-bros.jpg"
-        className="card-img-top"
-        alt="warner bross img"
-      />
+      <img src={image_url} className="card-img-top" alt={title} />
       <div className="card-body">
-        <h5 className="card-title">Titolo del film</h5>
+        <h5 className="card-title">{title}</h5>
         <address>
-          <i>Regista</i>
+          <i>By {author}</i>
         </address>
-        <p className="card-text">
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Soluta
-          ratione veritatis sunt molestias nihil odit totam, quis corporis
-          laboriosam excepturi. Labore vero molestiae eveniet explicabo esse
-          iure laborum nemo odio.
-        </p>
-        <Link to={`movies/1`} className="btn btn-primary">
+        <p className="card-text">{abstract}</p>
+        <Link to={`movies/${id}`} className="btn btn-primary">
           See more
         </Link>
       </div>
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,16 +1,61 @@
+import axios from "axios";
+import { useState, useEffect } from "react";
+
 import MovieCard from "../components/MovieCard";
 
+const endpoint = "http://localhost:3000/api/movies";
+
 const HomePage = () => {
+  // creo var di stato per gestire l'array di film restituiti dalla richiesta HTTP all'API
+  const [movies, setMovies] = useState([]);
+
+  const fetchMovies = () => {
+    axios
+      .get(endpoint)
+      .then((res) => {
+        // setMovies(res.data);
+        const moviesPlusUrl = res.data.map((movie) => ({
+          ...movie,
+          image_url: `http://localhost:3000/${movie.image}`,
+        }));
+        setMovies(moviesPlusUrl);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  // // funzione di rendering del listato dei film
+  const renderMovies = () => {
+    return movies.map((movie) => {
+      return (
+        <div
+          className="card-container d-flex flex-wrap homepage"
+          key={movie.id}
+        >
+          <MovieCard movieProp={movie} />
+        </div>
+      );
+    });
+  };
+
+  // richiamo fetchMovie una volta sola, al montaggio della pagina
+  useEffect(() => {
+    fetchMovies();
+  }, []);
+
   return (
     <>
       <h1 className="mt-3">Welcome to "Streaming Community" Website</h1>
       <h3 className="subtitle ">The world's largest pirate film archive</h3>
       <div className="card-container d-flex flex-wrap homepage">
+        {/* <MovieCard />
         <MovieCard />
         <MovieCard />
         <MovieCard />
-        <MovieCard />
-        <MovieCard />
+        <MovieCard /> */}
+
+        {renderMovies()}
       </div>
     </>
   );
