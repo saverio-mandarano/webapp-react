78ac86d89de000f36c505980c11633765affe572
saverio mandarano <saverio.mandarano1@gmail.com>
2026-02-26 17:14:21 +0100

    Add loader visibile su qualunque pagina dell'app

---
+++ src/components/Loader.jsx
@@ -0,0 +1,9 @@
+const Loader = () => {
+  return (
+    <div className="overlay">
+      <div className="spinner-border text-primary" role="status"></div>
+    </div>
+  );
+};
+
+export default Loader;
--- src/layouts/DefaultLayout.jsx
+++ src/layouts/DefaultLayout.jsx
@@ -4,6 +4,11 @@ import { Outlet } from "react-router-dom";
 //import navbar per averlo nel template che si ripeterà ad ogni sezione
 import NavBar from "../components/NavBar";
 
+//  import del loader
+import Loader from "../components/Loader";
+
+const isLoading = true;
+
 export default function DefaultLayout() {
   return (
     <>
@@ -14,6 +19,7 @@ export default function DefaultLayout() {
       <main className="container">
         <Outlet />
       </main>
+      {isLoading && <Loader />}
     </>
   );
 }
